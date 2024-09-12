6d23ce43d51480d4d68d987a462aa73b8cf98b96
robiuzzaman4 <robiuzzaman4@gmail.com>
2024-09-12 14:56:31 +0600

    feat: cart page initialized

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,14 +1,14 @@
 import Container from "@/components/ui/container";
 import React from "react";
 
-const HomePage = () => {
+const CartPage = () => {
   return (
     <section className="w-full py-16">
       <Container>
-        <h1 className="text-3xl font-medium tracking-tighter">Home Page</h1>
+        <h1 className="text-3xl font-medium tracking-tighter">Cart Page</h1>
       </Container>
     </section>
   );
 };
 
-export default HomePage;
+export default CartPage;
